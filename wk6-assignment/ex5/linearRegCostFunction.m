6e2fe014558196e955b7dfc7a2a96f49082bad4b
Aziz Javed <azizjaved@azizs-mbp.lan>
2017-08-16 07:42:40 -0500

    wk6 hw complete

--- wk6-assignment/ex5/linearRegCostFunction.m
+++ wk6-assignment/ex5/linearRegCostFunction.m
@@ -19,19 +19,10 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
-
-
-
-
-
-
-
-
+predictions = X * theta;
+J = 1 / 2 / m * sum((predictions - y).^2) + lambda / 2 / m * sum(theta(2:end, :).^2);
+grad = 1 / m * ((predictions - y)' * X)' + lambda / m * [0; theta(2:end, :)];
 % =========================================================================
-
 grad = grad(:);
 
 end
